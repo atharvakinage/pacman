354cfe7faba2e858c2c05f2a4731468e6ddfe7fc
abs109 <mtenabs@gmail.com>
2025-04-20 16:52:59 +0530

    Update PlayerFactory.java

--- src/main/java/com/example/pacman/factory/PlayerFactory.java
+++ src/main/java/com/example/pacman/factory/PlayerFactory.java
@@ -6,7 +6,6 @@ import com.example.pacman.model.Player;
 public class PlayerFactory implements EntityFactory {
     @Override
     public GameEntity createEntity(String type) {
-        // For simplicity, ignore `type` for now (needed for interface compatibility).
         return new Player(315, 270);
     }
 
